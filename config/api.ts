da657607f09b0c587fe4a879ed663c19b5dee3fa
Sahil raj <rajsahil3324@gmail.com>
2026-01-11 19:42:16 +0530

    updated-url

--- config/api.ts
+++ config/api.ts
@@ -1,5 +1,5 @@
 // const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000';
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://job-agent-backend.vercel.app/';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://job-agent-backend.vercel.app';
 
 // --- Types matching Backend Schema ---
 
